1e93cd45011bb9a8e912a70c725a0025f1b86c46
uo284414 <uo284414@uniovi.es>
2024-03-14 12:03:08 +0100

    SDI-2324-406-9.3-Subida a ficheros y gestión de colecciones.

--- musicstoreapp/repositories/songsRepository.js
+++ musicstoreapp/repositories/songsRepository.js
@@ -7,6 +7,7 @@ module.exports = {
         this.dbClient = dbClient;
         this.app = app;
     },
+
     insertSong: function (song, callbackFunction) {
         this.dbClient.connect()
             .then(() => {
@@ -18,5 +19,29 @@ module.exports = {
                     .catch(err => callbackFunction({error: err.message}));
             })
             .catch(err => callbackFunction({error: err.message}))
-    }
+    },
+
+    getSongs: async function (filter, options) {
+        try {
+            await this.dbClient.connect();
+            const database = this.dbClient.db(this.database);
+            const songsCollection = database.collection(this.collectionName);
+            const songs = await songsCollection.find(filter, options).toArray();
+            return songs;
+        } catch (error) {
+            throw (error);
+        }
+    },
+
+    findSong: async function (filter, options) {
+        try {
+            await this.dbClient.connect();
+            const database = this.dbClient.db(this.database);
+            const songsCollection = database.collection(this.collectionName);
+            const song = await songsCollection.findOne(filter, options);
+            return song;
+        } catch (error) {
+            throw (error);
+        }
+    },
 }
--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -1,3 +1,4 @@
+const {ObjectId} = require("mongodb");
 module.exports = function (app, songsRepository) {
     app.get("/songs", function (req, res) {
         let songs = [{
@@ -20,7 +21,7 @@ module.exports = function (app, songsRepository) {
     });
 
     app.get('/songs/add', function (req, res) {
-        res.render("add.twig");
+        res.render("songs/add.twig");
     });
 
     app.post("/songs/add", function(req,res) {
@@ -31,14 +32,54 @@ module.exports = function (app, songsRepository) {
         }
 
         songsRepository.insertSong(song, function(result){
-            if (result.songId !== null && result.songId !== undefined){
-                res.send("Agragada la canción ID: " + result.songId);
+            if (result !== null && result !== undefined) {
+                if (req.files != null) {
+                    let image = req.files.cover;
+                    image.mv(app.get("uploadPath") + '/public/covers/' + result + '.png')
+                        .then(() => {
+                            if (req.files.audio != null) {
+                                let audio = req.files.audio;
+                                audio.mv(app.get("uploadPath") + '/public/audios/' + result + '.mp3')
+                                    .then(res.send("Agregada la canción ID: " + result))
+                                    .catch(error => res.send("Error al subir el audio de la canción"))
+                            }else {
+                                res.send("Agregada la canción ID: " + result)
+                            }
+                        })
+                        .catch(error => res.send("Error al subir la portada de la canción " + result.error ))
+                } else {
+                    res.send("Agregada la canción ID: " + result)
+
+                }
             } else {
-                res.send("Error al insertar la canción " + result.error);
+                res.send("Error al insertar canción " + result.error);
             }
         });
     });
 
+    app.get('/shop', function(req, res) {
+        let filter = {};
+        let options = {sort: { title: 1}};
+        if(req.query.search != null && typeof(req.query.search) != "undefined" && req.query.search != "") {
+            filter = {"title": {$regex: ".*" + req.query.search + ".*"}};
+        }
+
+            songsRepository.getSongs(filter, options).then(songs => {
+            res.render("shop.twig", {songs:songs});
+        }).catch(error => {
+            res.send("Se ha producido un error al listar las canciones " + error)
+        });
+    });
+
+    app.get('/songs/:id', function (req, res) {
+        let filter = {_id: new ObjectId(req.params.id)};
+        let options = {};
+        songsRepository.findSong(filter, options).then(song => {
+            res.render("songs/song.twig", {song: song});
+        }).catch(error => {
+            res.send("Se ha producido un error al buscar la canción " + error)
+        });
+    })
 
     app.get('/songs/:id', function(req, res) {
         let response = 'id: ' + req.params.id;
